0d146662b18ac4329dc07e33fbe1307b3bca71f3
dav <andrei-dmitriev@ya.ru>
2023-06-28 10:23:15 +0300

    Add M3VS2 sample

---
+++ src/m3vs2/MyHome.java
@@ -0,0 +1,7 @@
+package m3vs2;
+public class MyHome {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/m3vs2/university/Student.java
@@ -0,0 +1,14 @@
+package m3vs2.university;
+
+public class Student {
+    int x = 0;
+
+    public void sayName(){
+        System.out.println("My name is Andrey");
+    }
+    private int y = 0;
+    static {
+        System.out.println("Hello");
+    }
+}
+
