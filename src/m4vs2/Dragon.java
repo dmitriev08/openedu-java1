2de06cceb08d811ab0f23207af2cc2a92971e966
dav <andrei-dmitriev@ya.ru>
2023-06-28 09:51:26 +0300

    Add M4VS2 sample

---
+++ src/m4vs2/Dragon.java
@@ -0,0 +1,11 @@
+package m4vs2;
+
+public class Dragon {
+    public String name;
+    public Dragon(){
+    }
+
+    public void sayName() {
+        System.out.println("Hello, my name is " + name);
+    }
+}
---
+++ src/m4vs2/Main.java
@@ -0,0 +1,9 @@
+package m4vs2;
+
+public class Main {
+    public static void main(String[] args) {
+        Dragon myLovelyDragon = new Dragon();
+        myLovelyDragon.name = "Кеша";
+        myLovelyDragon.sayName();
+    }
+}
