53d86811e3b115feffacb98c40d19b1d6f99df4f
dav <andrei-dmitriev@ya.ru>
2023-06-28 09:56:48 +0300

    Add M4VS4 sample

---
+++ src/m4vs4/Dragon.java
@@ -0,0 +1,11 @@
+package m4vs4;
+
+public class Dragon {
+    public String name;
+    public Dragon(){
+    }
+
+    public void sayName() {
+        System.out.println("Hello, my name is " + name);
+    }
+}
