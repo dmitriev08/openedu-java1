03faee7a9507fb7befb8dd884285bb085232ad83
dav <andrei-dmitriev@ya.ru>
2023-06-29 15:35:29 +0300

    Add M1VS2 sample

---
+++ src/m1vs2/Hello.java
@@ -0,0 +1,8 @@
+package m1vs2;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+//        System.out.println("Hello, " + args[0]);
+    }
+}
