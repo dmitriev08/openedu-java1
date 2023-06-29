9a84820971a4dc567a0e566b1955e0ca7faa23ae
dav <andrei-dmitriev@ya.ru>
2023-06-29 12:45:48 +0300

    Add M2VS2 sample

---
+++ src/m2vs2/Main.java
@@ -0,0 +1,15 @@
+package m2vs2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        int value = scanner.nextInt();
+//        System.out.println(">>" + value);
+        String s = scanner.nextLine();
+        System.out.println(">>" + s);
+
+    }
+
+}
